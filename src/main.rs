29c425105eca031448404672e6f39ed15f22f84a
merq312 <merc312@gmail.com>
2022-04-10 12:11:33 -0500

    Chapter 4

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
-use rltk::{GameState, RGB, Rltk};
+use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
 
 pub use components::*;
 pub use map::*;
 pub use player::*;
+pub use rect::Rect;
 
-mod rect;
-mod player;
-mod map;
 mod components;
+mod map;
+mod player;
+mod rect;
 
 pub struct State {
     ecs: World,
 }
 
-impl State {
-    fn run_systems(&mut self) {
-        let mut lw = LeftWalker {};
-        lw.run_now(&self.ecs);
-        self.ecs.maintain();
-    }
-}
+// impl State {
+//     fn run_systems(&mut self) {
+//         let mut lw = LeftWalker {};
+//         lw.run_now(&self.ecs);
+//         self.ecs.maintain();
+//     }
+// }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
+        // ctx.print(1, 1, "Hello Rust World");
 
         player_input(self, ctx);
-        self.run_systems();
+        // self.run_systems();
 
         let map = self.ecs.fetch::<Vec<TileType>>();
         draw_map(&map, ctx);
@@ -50,13 +51,19 @@ fn main() -> rltk::BError {
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
-    gs.ecs.insert(new_map());
+    // gs.ecs.register::<LeftMover>();
+
+    let (rooms, map) = new_map_rooms_and_corridors();
+    gs.ecs.insert(map);
+    let (player_x, player_y) = rooms[0].center();
 
     gs.ecs
         .create_entity()
-        .with(Position { x: 40, y: 25 })
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
@@ -65,18 +72,18 @@ fn main() -> rltk::BError {
         .with(Player {})
         .build();
 
-    for i in 0..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: rltk::to_cp437('☺'),
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            // .with(LeftMover {})
-            .build();
-    }
+    // for i in 0..10 {
+    //     gs.ecs
+    //         .create_entity()
+    //         .with(Position { x: i * 7, y: 20 })
+    //         .with(Renderable {
+    //             glyph: rltk::to_cp437('☺'),
+    //             fg: RGB::named(rltk::RED),
+    //             bg: RGB::named(rltk::BLACK),
+    //         })
+    //         // .with(LeftMover {})
+    //         .build();
+    // }
 
     rltk::main_loop(context, gs)
 }
--- src/player.rs
+++ src/player.rs
@@ -3,7 +3,7 @@ use std::cmp::{max, min};
 use rltk::{Rltk, VirtualKeyCode};
 use specs::prelude::*;
 
-use super::{Player, Position, State, TileType, xy_idx};
+use super::{xy_idx, Player, Position, State, TileType};
 
 pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
@@ -23,12 +23,23 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
     match ctx.key {
         None => {} // Nothing happened
         Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            VirtualKeyCode::Left | VirtualKeyCode::Numpad4 | VirtualKeyCode::H => {
+                try_move_player(-1, 0, &mut gs.ecs)
+            }
+
+            VirtualKeyCode::Right | VirtualKeyCode::Numpad6 | VirtualKeyCode::L => {
+                try_move_player(1, 0, &mut gs.ecs)
+            }
+
+            VirtualKeyCode::Up | VirtualKeyCode::Numpad8 | VirtualKeyCode::K => {
+                try_move_player(0, -1, &mut gs.ecs)
+            }
+
+            VirtualKeyCode::Down | VirtualKeyCode::Numpad2 | VirtualKeyCode::J => {
+                try_move_player(0, 1, &mut gs.ecs)
+            }
+
             _ => {}
         },
     }
 }
-
